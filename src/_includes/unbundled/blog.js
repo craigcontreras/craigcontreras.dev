01708b1b3bc16b914c534ca63cb0fc56ba8be77c
Craig Contreras <craig1contreras@gmail.com>
2022-01-30 02:18:56 -0500

    finally production

--- src/_includes/unbundled/blog.js
+++ src/_includes/unbundled/blog.js
@@ -11,3 +11,13 @@ const headerAnim = lottie.loadAnimation({
     autoplay: true,
     path: '/assets/json/blog-header.json'
 });
+
+document.querySelector("#top-post").addEventListener("click", e => {
+    let target = e.target.querySelector('a').href;
+    console.log(target);
+                
+    document.querySelector(".transition").classList.add("is-active");
+    setTimeout(() => {
+        window.location.replace(target);
+    }, 500);
+});
